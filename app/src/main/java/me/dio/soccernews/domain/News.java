638bd0395a34d59b1602aff5a429baaf66e7af5e
Luana <luana.mouro@gmail.com>
2022-07-12 17:06:15 -0300

    Alteracoes layout

---
+++ app/src/main/java/me/dio/soccernews/domain/News.java
@@ -0,0 +1,22 @@
+package me.dio.soccernews.domain;
+
+public class News {
+    private string NewsTitle;
+    private string News;
+
+    public string getNewsTitle() {
+        return NewsTitle;
+    }
+
+    public void setNewsTitle(string newsTitle) {
+        NewsTitle = newsTitle;
+    }
+
+    public string getNews() {
+        return News;
+    }
+
+    public void setNews(string news) {
+        News = news;
+    }
+}
