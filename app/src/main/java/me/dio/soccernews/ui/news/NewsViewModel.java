9fbafd03357f972c22af58ac3f808a7b416a7317
Luana <luana.mouro@gmail.com>
2022-07-20 14:36:28 -0300

    Alteracoes video 12_02

--- app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/me/dio/soccernews/ui/news/NewsViewModel.java
@@ -9,6 +9,9 @@ import java.util.List;
 
 import me.dio.soccernews.data.remote.SoccerNewsApi;
 import me.dio.soccernews.domain.News;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -24,6 +27,27 @@ public class NewsViewModel extends ViewModel {
                 .build();
 
         api = retrofit.create(SoccerNewsApi.class);
+        findNews();
+    }
+
+    private void findNews() {
+        api.getNews().enqueue(new Callback<List<News>>()
+        {
+            @Override
+            public void onResponse(Call<List<News>> call, Response<List<News>> response) {
+                if(response.isSuccessful()){
+                    news.setValue(response.body());
+
+                } else {
+                    // todo: pensar em uma estratégia para tratamento de erros.
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<News>> call, Throwable t) {
+            // todo: pensar em uma estratégia para tratamento de erros.
+            }
+        });
     }
 
     public LiveData<List<News>> getNews() {
